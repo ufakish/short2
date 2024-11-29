05108f016d97dc8c96efc303575d77e6fe47b505
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2024-11-29 21:55:47 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -232,7 +232,7 @@ async def main():
 
 
 
-# Some Functions For Paste ...!!
+# Some Required Functions ...!!
 
 
 def _netcat(host, port, content):
@@ -252,6 +252,32 @@ async def paste_queue(content):
     loop = asyncio.get_running_loop()
     link = await loop.run_in_executor(None, partial(_netcat, "ezup.dev", 9999, content))
     return link
+
+
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for i in range(len(time_list)):
+        time_list[i] = str(time_list[i]) + time_suffix_list[i]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
+
 
 
 
